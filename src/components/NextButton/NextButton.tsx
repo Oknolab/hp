a2df8754a2f2ee67184ebdcdc382f78d8bb9133b
nyantama0616 <g2124029@fun.ac.jp>
2024-10-01 23:28:49 +0900

    NextButtonのドラフト完成

--- src/components/NextButton/NextButton.tsx
+++ src/components/NextButton/NextButton.tsx
@@ -1,5 +1,35 @@
-export const NextButton = () => {
+import { Transition } from '@headlessui/react';
+
+type NextButtonProps = {
+  isShowing?: boolean;
+};
+
+export const NextButton = ({ isShowing = false }: NextButtonProps) => {
   return (
-    <button className="bg-red-200">Next</button>
+    <button className="flex h-12 w-20 items-center justify-center">
+      <Transition
+        enter="transition-transform duration-500"
+        enterFrom="scale-0"
+        enterTo="scale-100"
+        leave="transition-transform duration-200"
+        leaveFrom="scale-100"
+        leaveTo="scale-0"
+        show={isShowing}
+      >
+        <div className="flex size-full items-center justify-center border-2 p-1">
+          <span className="mr-2">Next</span>
+          <svg
+            className="size-4"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M9 5l7 7-7 7" strokeLinecap="round" strokeLinejoin="round"></path>
+          </svg>
+        </div>
+      </Transition>
+    </button>
   );
-}
+};
