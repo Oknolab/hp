14267d6f877b8dcd0499ee3909a1221e1f0fe256
nyantama0616 <g2124029@fun.ac.jp>
2024-10-01 11:59:39 +0900

    jsxのpropsをソートするようにeslintを設定

--- eslint.config.js
+++ eslint.config.js
@@ -6,50 +6,59 @@ import tseslint from 'typescript-eslint'
 
 import tailwindcss from 'eslint-plugin-tailwindcss'
 import importPlugin from "eslint-plugin-import";
+import react from 'eslint-plugin-react';
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ['dist'] },
   {
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      ...tailwindcss.configs["flat/recommended"],
+      ...tailwindcss.configs['flat/recommended'],
     ],
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      "tailwindcss": tailwindcss,
-      "import": importPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      tailwindcss: tailwindcss,
+      import: importPlugin,
+      react: react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "import/order": [
-        "error",
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'import/order': [
+        'error',
         {
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            "parent",
-            "sibling",
-            "index",
-          ],
-          "newlines-between": "always",
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
           alphabetize: {
-            order: "asc",
+            order: 'asc',
             caseInsensitive: true,
           },
         },
       ],
+      'react/jsx-sort-props': [
+        'error',
+        { callbacksLast: true, shorthandFirst: true, ignoreCase: true, reservedFirst: true },
+      ], // JSXのpropsをソートする
+      'react/jsx-wrap-multilines': [
+        'error',
+        {
+          declaration: 'parens-new-line',
+          assignment: 'parens-new-line',
+          return: 'parens-new-line',
+          arrow: 'parens-new-line',
+          condition: 'parens-new-line',
+          logical: 'parens-new-line',
+          prop: 'parens-new-line',
+        },
+      ],
+      'import/no-default-export': 'warn', // default exportを禁止する
     },
   }
 );
