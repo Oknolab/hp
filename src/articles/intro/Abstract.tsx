7541ce6c5cf85668ac83c7ce08ef52d2d97a7170
中北竜馬 <b1020183@fun.ac.jp>
2024-10-05 23:46:03 +0900

    概要記事作った

---
+++ src/articles/intro/Abstract.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@/components';
+
+export const Abstract = () => {
+  return (
+    <>
+      <Typography variant="h1">概要</Typography>
+      <p>Abstract text</p>
+    </>
+  );
+};
