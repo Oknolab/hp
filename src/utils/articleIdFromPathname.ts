9f9fd211e0bb982028214c369565c68d30996598
nyantama0616 <g2124029@fun.ac.jp>
2024-10-08 11:59:26 +0900

    faviconとか置いた

--- src/utils/articleIdFromPathname.ts
+++ src/utils/articleIdFromPathname.ts
@@ -1,4 +1,7 @@
 export function articleIdFromPathname(pathname: string): string | null {
+  if (pathname === '/') {
+    return 'intro-abstract'; //temp
+  }
   const n = pathname.length;
   return pathname.slice(1, n).replace('/', '-') || null;
 }
