e8f8e1ba9f7d93ffcd26c5e5d99b96bdc14ba8cb
nyantama0616 <g2124029@fun.ac.jp>
2024-10-21 00:27:09 +0900

    UserListにkeyなかったから追加

--- src/components/user/UserList/UserList.tsx
+++ src/components/user/UserList/UserList.tsx
@@ -10,8 +10,8 @@ export const UserList = ({ users }: UserListProps) => {
   return (
     <div className="grid grid-cols-4 gap-8">
       {users.map((user) => (
-        <div className="col-span-1 flex h-64 overflow-hidden border">
-          <UserAbstract key={user.userId} user={user} />
+        <div key={user.userId} className="col-span-1 flex h-64 overflow-hidden border">
+          <UserAbstract user={user} />
         </div>
       ))}
     </div>
