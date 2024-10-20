5cacb1c901c02aeeb376756c32955280c4729272
nyantama0616 <g2124029@fun.ac.jp>
2024-10-21 03:55:21 +0900

    okudaくんのデータ反映した！

--- src/mocks/users/okuda.ts
+++
@@ -1,9 +0,0 @@
-import { UserDetail } from '@/types';
-
-export const okuda: UserDetail = {
-  userId: 'okuda',
-  nickName: 'アンチパンダ',
-  avatarUrl: '/images/user/default.png',
-  description: 'パンダ組じゃないです',
-  twitterId: undefined,
-};
---
+++ src/mocks/users/okuda.tsx
@@ -0,0 +1,17 @@
+import { Typography } from '@/components';
+import { UserDetail } from '@/types';
+
+export const okuda: UserDetail = {
+  userId: 'okuda',
+  nickName: '拓真',
+  avatarUrl: '/images/user/default.png',
+  description: (
+    <>
+      <Typography>拓真です！</Typography>
+      <Typography>ゲームが大好きです。</Typography>
+      <Typography>ボードゲームもカードゲームも好きです。</Typography>
+      <Typography>あと文化財とクイズゲームを掛け合わせた研究をしてます。</Typography>
+    </>
+  ),
+  twitterId: 'FUN2021_takuma',
+};
