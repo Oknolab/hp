09a29aa3845fadd4eb03eda1722e03469e16ce6d
nyantama0616 <g2124029@fun.ac.jp>
2024-10-21 01:36:31 +0900

    他の研究分野にもList置いた

--- src/articles/research/Archive.tsx
+++ src/articles/research/Archive.tsx
@@ -1,9 +1,12 @@
 import { ArticleLayout, Typography } from '@/components';
+import { ResearchList } from '@/components/research';
+import { tourisms } from '@/mocks';
 
 export const Archive = () => {
   return (
     <ArticleLayout title="デジタルアーカイブ">
       <Typography>デジタルアーカイブとは、電子データを頑張って貯蔵するための学問です。</Typography>
+      <ResearchList researches={tourisms} />
     </ArticleLayout>
   );
 };
