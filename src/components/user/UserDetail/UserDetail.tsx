8a529a1168860bf7e91aa3e630113cdaf734aec3
nyantama0616 <g2124029@fun.ac.jp>
2024-10-21 00:31:01 +0900

    UserDetailにTwitterボタン追加

--- src/components/user/UserDetail/UserDetail.tsx
+++ src/components/user/UserDetail/UserDetail.tsx
@@ -9,8 +9,32 @@ export const UserDetail = ({ user }: UserDetailProps) => {
   return (
     <div>
       <img alt="user image" className="w-24 rounded-full" src={user.avatarUrl} />
-      <DialogTitle className="text-center">{user.nickName}</DialogTitle>
+      <div>
+        <DialogTitle className="text-center">{user.nickName}</DialogTitle>
+        <TwitterButton twitterId={user.twitterId} />
+      </div>
       <Description>{user.description}</Description>
     </div>
+  );
+};
+
+type TwitterButtonProps = {
+  twitterId?: string;
+};
+
+export const TwitterButton = ({ twitterId }: TwitterButtonProps) => {
+  if (!twitterId) {
+    return null;
+  }
+
+  return (
+    <a
+      className="block w-24 rounded-md border border-gray-300 p-2 text-center"
+      href={`https://x.com/${twitterId}`}
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      Twitter
+    </a>
   );
 };
