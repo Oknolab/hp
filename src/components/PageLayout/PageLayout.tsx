8ed44391b3405a012466b708611b41fbc30ada4b
nyantama0616 <g2124029@fun.ac.jp>
2024-10-21 02:28:07 +0900

    背景色をクリーム色にしてみた

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -5,7 +5,7 @@ type PageLayoutProps = {
 };
 export const PageLayout = ({ children }: PageLayoutProps) => {
   return (
-    <div className="flex w-full justify-center">
+    <div className="flex size-full justify-center bg-cream">
       {/* TODO: tailwind.configで設定したい */}
       <div className="flex w-full max-w-[1080px] justify-center gap-4 border p-4">
         <div className="flex grow flex-col gap-4">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.tsx"
-  ],
+  content: ['./index.html', './src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: 'rgba(255, 237, 179, 0.6)',
+      },
+    },
   },
   plugins: [],
-}
+};
