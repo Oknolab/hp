26a4fd80c78eac836609fab001c84cbf0550aa42
nyantama0616 <g2124029@fun.ac.jp>
2024-10-21 00:12:12 +0900

    UserAbstractクリックできるようにした

--- src/components/user/UserAbstract/UserAbstract.tsx
+++ src/components/user/UserAbstract/UserAbstract.tsx
@@ -7,15 +7,21 @@ type UserAbstractProps = {
   user: User;
 };
 export const UserAbstract = ({ user }: UserAbstractProps) => {
-  const className = clsx('w-full rounded-md border p-4', 'flex flex-col items-center gap-2');
+  const className = clsx(
+    'w-full rounded-md p-3',
+    'flex flex-col items-center gap-2',
+    'border-gray-300 bg-white shadow-lg',
+    'cursor-pointer transition-colors duration-200 hover:bg-gray-100 hover:shadow-xl',
+    'text-left'
+  );
 
   return (
-    <div className={className}>
+    <button className={className}>
       <img alt="user image" className="w-24 rounded-full" src={user.avatarUrl} />
       <Typography>{user.nickName}</Typography>
       <div className="w-full overflow-hidden">
         <Typography>{user.description}</Typography>
       </div>
-    </div>
+    </button>
   );
 };
