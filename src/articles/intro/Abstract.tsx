7e43f5b3f1cd875ceea23d4e7989f65752e96f87
nyantama0616 <g2124029@fun.ac.jp>
2024-10-21 15:56:55 +0900

    こめじ置いた

--- src/articles/intro/Abstract.tsx
+++ src/articles/intro/Abstract.tsx
@@ -5,7 +5,9 @@ export const Abstract = () => {
     <ArticleLayout title="概要">
       <div>
         <Typography>比較的自由な研究室だと思います。</Typography>
+        <Typography>興味のあることをやらせてくれて、たくさんダメ出ししてもらえます。</Typography>
       </div>
+      <img alt="こめじ" src="/images/komeji_happy.jpg" />
     </ArticleLayout>
   );
 };
