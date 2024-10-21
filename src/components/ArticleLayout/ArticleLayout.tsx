dba9a8be63cfc3286c049191e1ed51f74ef808c2
nyantama0616 <g2124029@fun.ac.jp>
2024-10-21 14:08:31 +0900

    Articleをスクロールするようにした

--- src/components/ArticleLayout/ArticleLayout.tsx
+++ src/components/ArticleLayout/ArticleLayout.tsx
@@ -8,11 +8,7 @@ type ArticleLayoutProps = {
   children: React.ReactNode;
 };
 export const ArticleLayout = ({ title, className, children }: ArticleLayoutProps) => {
-  const _className = clsx(
-    'prose rounded-md border p-4',
-    'min-h-[800px]', //temp
-    className
-  );
+  const _className = clsx('prose rounded-md border p-4', 'shadow-lg', className);
   return (
     <article className={_className}>
       <Typography variant="h1">{title}</Typography>
