ea77885726bf7fd5dd2adb9e92e639ad3cdca4eb
nyantama0616 <g2124029@fun.ac.jp>
2024-10-24 19:49:02 +0900

    くまさんAppのリンク消した

--- src/articles/research/Tourism.tsx
+++ src/articles/research/Tourism.tsx
@@ -1,12 +1,11 @@
 import { ArticleLayout, Typography } from '@/components';
 import { ResearchList } from '@/components/research';
-import { tourisms } from '@/mocks';
 
 export const Tourism = () => {
   return (
     <ArticleLayout title="観光情報学">
       <Typography>観光情報学とは、観光をシステムで支援するための学問です。</Typography>
-      <ResearchList researches={tourisms} />
+      <ResearchList researches={[]} />
     </ArticleLayout>
   );
 };
