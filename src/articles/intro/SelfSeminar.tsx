1ffd9df7dd96cbeacd9e47d1c84f2f4f003cf5e1
nyantama0616 <g2124029@fun.ac.jp>
2024-10-26 13:39:58 +0900

    自主ゼミページに歴代テーマのリンク貼った

--- src/articles/intro/SelfSeminar.tsx
+++ src/articles/intro/SelfSeminar.tsx
@@ -5,6 +5,16 @@ export const SelfSeminar = () => {
     <ArticleLayout title="自主ゼミはどんな感じ？">
       <Typography>学生主体で、学びたいことに取り組みます。</Typography>
       <Typography>先輩が色々教えてくれます。</Typography>
+      <p>
+        過去の自主ゼミのテーマは
+        <a
+          className="text-blue-500 hover:opacity-60"
+          href="https://hope.fun.ac.jp/mod/page/view.php?id=54335"
+        >
+          こちら
+        </a>
+        からご確認ください。
+      </p>
     </ArticleLayout>
   );
 };
