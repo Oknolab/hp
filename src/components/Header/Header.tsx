c5a5433d70bad92378a9e083d656ad05871582e4
nyantama0616 <g2124029@fun.ac.jp>
2024-10-26 13:18:42 +0900

    ロゴの位置整備した

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,23 +4,20 @@ import { Link } from 'react-router-dom';
 import { paths } from '@/utils';
 
 export const Header = () => {
-  const className = clsx(
-    'fixed left-0 top-0 z-50 flex w-full justify-between bg-white p-2',
-    'font-okunolab'
-  );
+  const className = clsx('fixed left-0 top-0 z-50  w-full bg-white py-2', 'flex justify-center');
 
   return (
     <header className={className}>
-      <Logo />
+      <div className="w-full max-w-[1080px] px-4">
+        <Logo />
+      </div>
     </header>
   );
 };
 
 const Logo = () => {
-  const className = clsx('px-2 text-4xl font-bold text-[#072ff5] ');
-
   return (
-    <Link className={className} to={paths.top}>
+    <Link to={paths.top}>
       <img alt="logo" className="w-32" src="/logo.png" />
     </Link>
   );
