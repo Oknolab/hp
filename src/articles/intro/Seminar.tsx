aed5e473e839a548edfc5e03abf29b40fd0cdc1f
nyantama0616 <g2124029@fun.ac.jp>
2024-10-27 14:26:12 +0900

    languageをjaに変更

--- src/articles/intro/Seminar.tsx
+++ src/articles/intro/Seminar.tsx
@@ -4,7 +4,7 @@ export const Seminar = () => {
   return (
     <ArticleLayout title="ゼミはどんな感じ？">
       <Typography>ゼミはメンバー全員で行います。</Typography>
-      <Typography>一人が発表したと、他のメンバーが意見を出し合います。</Typography>
+      <Typography>一人が発表したあと、他のメンバーが意見を出し合います。</Typography>
       <Typography>全員が発言権を持てるように、心理的安全性を重視します。</Typography>
       <Typography>先輩がサポートします。</Typography>
     </ArticleLayout>
