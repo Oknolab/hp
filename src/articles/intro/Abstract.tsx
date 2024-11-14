16128a06cd5f3cbe21059d56c02e3427dab7fcfc
nyantama0616 <g2124029@fun.ac.jp>
2024-11-14 11:53:53 +0900

    Abstractに追記

--- src/articles/intro/Abstract.tsx
+++ src/articles/intro/Abstract.tsx
@@ -7,7 +7,13 @@ export const Abstract = () => {
         <Typography>比較的自由な研究室だと思います。</Typography>
         <Typography>興味のあることをやらせてくれて、たくさんダメ出ししてもらえます。</Typography>
       </div>
-      <img alt="こめじ" src="/images/komeji_happy.jpg" />
+      <div>
+        <Typography>
+          奥野研に興味ある方は、お気軽に卒研スペースの先輩に話しかけてみてください。
+        </Typography>
+        <Typography>「ホームページ見たよー！」って言われるとテンション爆上がりします↑↑</Typography>
+      </div>
+      <img alt="こめじ" src="/images/komeji_happy.jpg" width={500} />
     </ArticleLayout>
   );
 };
